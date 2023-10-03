332c39bac0a224b5142fef34eb18db438e586db2
noteolvides <gustavogomezlopezdesanroman@gmail.com>
2023-10-03 12:45:07 +0200

    refactor: update focus
    Signed-off-by: noteolvides <gustavogomezlopezdesanroman@gmail.com>

--- src/kaleidoscope/device/dygma/defy_wireless/Focus.h
+++ src/kaleidoscope/device/dygma/defy_wireless/Focus.h
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License along with
  * this program. If not, see <http://www.gnu.org/licenses/>.
  */
-#ifdef ARDUINO_NRF5_DEFY
+#ifdef ARDUINO_ARCH_NRF52
 
 #pragma once
 
