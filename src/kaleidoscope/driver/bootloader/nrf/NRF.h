59e6b93488c5c8a535f06a5741695ee841b389a5
noteolvides <gustavogomezlopezdesanroman@gmail.com>
2023-12-01 13:06:22 +0100

    fix: disable wdt
    Signed-off-by: noteolvides <gustavogomezlopezdesanroman@gmail.com>

--- src/kaleidoscope/driver/bootloader/nrf/NRF.h
+++ src/kaleidoscope/driver/bootloader/nrf/NRF.h
@@ -19,6 +19,7 @@ namespace nrf {
 class nrfBoot : public kaleidoscope::driver::bootloader::Base {
    public:
     static void rebootBootloader() {
+        EEPROM.erase();
         TinyUSBDevice.detach();
         bldrdrv_init();
         bldrdrv_update_request();
