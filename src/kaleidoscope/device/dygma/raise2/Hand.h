835e769833d9b383f147aaee1ccb16be47b3be10
gonza2206 <gonzalopez2206@gmail.com>
2024-02-22 17:11:36 -0300

    chore: update progress
    Signed-off-by: gonza2206 <gonzalopez2206@gmail.com>

--- src/kaleidoscope/device/dygma/raise2/Hand.h
+++ src/kaleidoscope/device/dygma/raise2/Hand.h
@@ -34,9 +34,9 @@ namespace device {
 namespace dygma {
 namespace raise2 {
 
-#define LED_BANKS           9
+#define LED_BANKS           11
 
-#define LEDS_PER_HAND       72
+#define LEDS_PER_HAND       88
 #define LPH                 LEDS_PER_HAND
 #define LEDS_PER_BANK       8
 #define LED_BYTES_PER_BANK  (sizeof(cRGB) * LEDS_PER_BANK)
