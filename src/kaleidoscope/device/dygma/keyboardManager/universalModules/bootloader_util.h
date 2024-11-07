19b4608ff006d3e82b38cc8fd84889a7a00dc2d5
Ota Fejfar <ota.fejfar@gmail.com>
2024-11-07 13:20:04 +0100

    chore: Adding license headers.
    Signed-off-by: Ota Fejfar <ota.fejfar@gmail.com>

--- src/kaleidoscope/device/dygma/keyboardManager/universalModules/bootloader_util.h
+++ src/kaleidoscope/device/dygma/keyboardManager/universalModules/bootloader_util.h
@@ -1,4 +1,22 @@
 
+/* -*- mode: c++ -*-
+ * kaleidoscope::device::dygma::Defy -- Kaleidoscope device plugin for Dygma Defy
+ * Copyright (C) 2017-2019  Keyboard.io, Inc
+ * Copyright (C) 2017-2019  Dygma Lab S.L.
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef __BOOTLOADER_UTIL_H_
 #define __BOOTLOADER_UTIL_H_
 
