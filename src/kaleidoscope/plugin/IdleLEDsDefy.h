234f4cd4b7e90c5a21d6b4044e7ac173ab7fb25a
gonza2206 <gonzalopez2206@gmail.com>
2025-03-10 12:58:53 -0300

    feat: Remove forced sleep in wired mode. Fix bug where the keyboard would immediately enter sleep mode when switching to wireless if LEDs were off in wired mode.
    Signed-off-by: gonza2206 <gonzalopez2206@gmail.com>

--- src/kaleidoscope/plugin/IdleLEDsDefy.h
+++ src/kaleidoscope/plugin/IdleLEDsDefy.h
@@ -37,14 +37,12 @@ class IdleLEDsDefy : public kaleidoscope::Plugin
             uint32_t sides_sleep_idle_t_ms;     // Timeout to put to sleep the keyboard sides [ms].
             uint32_t leds_off_usb_idle_t_ms;    // Power off time for LEDs, when the n2 is in USB mode [ms].
             uint32_t leds_off_ble_idle_t_ms;    // Power off time for LEDs, when the n2 is in BLE mode [ms].
-            uint32_t sides_sleep_idle_wired_t_ms; // Timeout to put to sleep the keyboard sides when wired [ms].
         };
 
         static IdleTime Power_save;
         static constexpr const uint32_t leds_off_usb_idle_t_ms_default = 600000;  // 600.000 ms = 10 minutes
         static constexpr const uint32_t leds_off_ble_idle_t_ms_default = 300000;  // 300.000 ms = 5 minutes
         static constexpr const uint32_t sides_sleep_idle_t_ms_default = 60000;    // 60.000 ms = 1 minutes
-        static constexpr const uint32_t sides_sleep_idle_wired_t_ms_default = 1800000; // 1.800.000 ms = 30 minutes
         
         static void save_power_save_settings(const IdleTime& data);
         static uint32_t ms_to_seconds(uint32_t time_in_ms);
