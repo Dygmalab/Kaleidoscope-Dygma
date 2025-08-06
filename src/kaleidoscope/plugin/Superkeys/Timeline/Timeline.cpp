b5e54c6ad462072c09d0a79e8cd1ff4d52665b2c
gonza2206 <gonzalopez2206@gmail.com>
2025-08-06 11:22:53 -0300

    feat: add initial Timeline class implementation with header and source files

---
+++ src/kaleidoscope/plugin/Superkeys/Timeline/Timeline.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Urano on 31/7/2025.
+//
+
+#include "Timeline.h"
---
+++ src/kaleidoscope/plugin/Superkeys/Timeline/Timeline.h
@@ -0,0 +1,16 @@
+//
+// Created by Urano on 31/7/2025.
+//
+
+#ifndef NEURONWIRELESS_DEFY_TIMELINE_H
+#define NEURONWIRELESS_DEFY_TIMELINE_H
+
+
+
+class Timeline {
+
+};
+
+
+
+#endif //NEURONWIRELESS_DEFY_TIMELINE_H
