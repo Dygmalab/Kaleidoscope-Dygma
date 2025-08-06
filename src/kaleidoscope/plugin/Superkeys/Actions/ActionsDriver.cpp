1c4a3e9d92b7b9a386da66e5b8c827f43e9588bd
gonza2206 <gonzalopez2206@gmail.com>
2025-08-06 11:23:49 -0300

    refactor: update return_type and action_handler signatures to use ExtendedActions for improved key handling

--- src/kaleidoscope/plugin/Superkeys/Actions/ActionsDriver.cpp
+++ src/kaleidoscope/plugin/Superkeys/Actions/ActionsDriver.cpp
@@ -1,115 +1,123 @@
 
 #include "ActionsDriver.h"
 
-Key ActionsDriver::return_type(uint8_t tap_count, const Key *actions, bool repeat_on_hold)
+Utils::ExtendedActions ActionsDriver::return_type(uint8_t tap_count, const Key *actions)
 {
+    Utils::ExtendedActions sk_key_action;
     switch (static_cast<Utils::TapType>(tap_count))
     {
     case Utils::TapType::None:
     {
-        NRF_LOG_DEBUG("None");
+        // NRF_LOG_DEBUG("None");
+        sk_key_action.key = 0xFFFF;
+        sk_key_action.key_is_interruptable = false;
         return {0xFFFF};
     }
-    case Utils::TapType::Hold_Once:
-    {
-        NRF_LOG_DEBUG("Hold_Once");
-        check_if_key_is_repeateable(actions[1]);
-        return actions[1];
-    }
+    break;
+
     case Utils::TapType::Tap_Once:
     {
-        NRF_LOG_DEBUG("Tap_Once");
-        return actions[0];
+        // NRF_LOG_DEBUG("Tap_Once");
+        sk_key_action.key = actions[0];
+        sk_key_action.key_is_interruptable = key_can_interrupt(actions[0]);
+        sk_key_action.key.setFlags(kaleidoscope::Runtime.hid().keyboard().isAnyModifierKeyActive());
     }
+    break;
+
+    case Utils::TapType::Hold_Once:
+    {
+        // NRF_LOG_DEBUG("Hold_Once");
+        sk_key_action.key = actions[1];
+        sk_key_action.key_is_interruptable = key_can_interrupt(actions[1]);
+    }
+    break;
+
     case Utils::TapType::Tap_Hold:
     {
-        NRF_LOG_DEBUG("Tap_Hold");
-        check_if_key_is_repeateable(actions[2]);
-        return actions[2];
+        // NRF_LOG_DEBUG("Tap_Hold");
+        sk_key_action.key = actions[2];
+        sk_key_action.key_is_interruptable = key_can_interrupt(actions[2]);
     }
+    break;
+
     case Utils::TapType::Tap_Twice:
     {
-        NRF_LOG_DEBUG("Tap_Twice");
-        return actions[3];
+        // NRF_LOG_DEBUG("Tap_Twice");
+        sk_key_action.key = actions[3];
+        sk_key_action.key_is_interruptable = key_can_interrupt(actions[3]);
     }
+    break;
+
     case Utils::TapType::Tap_Twice_Hold:
     {
-        NRF_LOG_DEBUG("Tap_Twice_Hold");
-        check_if_key_is_repeateable(actions[4]);
-        return actions[4];
+        // NRF_LOG_DEBUG("Tap_Twice_Hold");
+        sk_key_action.key = actions[4];
+        sk_key_action.key_is_interruptable = key_can_interrupt(actions[4]);
     }
+    break;
+
     default:
     {
-        NRF_LOG_DEBUG("Tap_Trice or more");
-        return actions[3];
+        // NRF_LOG_DEBUG("Tap_Trice or more");
+        sk_key_action.key = actions[3];
+        sk_key_action.key_is_interruptable = key_can_interrupt(actions[3]);
     }
+    break;
     }
+    return sk_key_action;
 }
 
-bool ActionsDriver::action_handler(uint8_t tap_count, const Key *actions, const Key &key, const KeyAddr &keyAddr, bool repeat_on_hold)
+void logKeyModifiers(Key key)
 {
+    uint8_t flags = key.getFlags();
+
+    if (flags & CTRL_HELD)
+        NRF_LOG_DEBUG("CTRL held");
+    if (flags & LALT_HELD)
+        NRF_LOG_DEBUG("Left ALT held");
+    if (flags & RALT_HELD)
+        NRF_LOG_DEBUG("Right ALT held");
+    if (flags & SHIFT_HELD)
+        NRF_LOG_DEBUG("SHIFT held");
+    if (flags & GUI_HELD)
+        NRF_LOG_DEBUG("GUI/Win held");
+    if (flags == 0)
+        NRF_LOG_DEBUG("No modifiers");
+}
+
+bool ActionsDriver::action_handler(uint8_t tap_count, const Key *actions, const Key &key, const KeyAddr &keyAddr)
+{
+    bool result = false;
     // TODO: Create Key list to filter the key and run the corresponding action.
     // TODO: Add a event type HOLD or TAP in order to decide if the HOLD action has to send serveral times or only once.
 
-    Key released_key = return_type(tap_count, actions, repeat_on_hold);
+    Utils::ExtendedActions returned_key = return_type(tap_count, actions);
 
-    NRF_LOG_DEBUG("Key relased %i", released_key.getRaw());
-    handleKeyswitchEvent(released_key, keyAddr, IS_PRESSED | INJECTED);
+    /*    NRF_LOG_DEBUG("Key released raw=%i flags=%i",
+                      returned_key.key.getRaw(),
+                      returned_key.key.getFlags());*/
 
-    kaleidoscope::Runtime.hid().keyboard().sendReport();
-    return true;
+    if (returned_key.key_is_interruptable)
+    {
+        result = true;
+    }
+
+    // logKeyModifiers(returned_key.key);
+
+    handleKeyswitchEvent(returned_key.key, keyAddr, IS_PRESSED | INJECTED);
+
+    return result;
 }
 
 bool ActionsDriver::isOnlyModifier(Key key)
 {
-    uint16_t raw = key.getRaw();
-    uint8_t modif = (raw & 0xFF00) >> 8;
-    uint8_t key_id = raw & 0x00FF;
+    uint16_t key_id = key.getRaw() & 0x00FF; // Tomamos sólo el HID keycode (parte baja)
 
-    return (key_id == Key_NoKey.getRaw() && modif != 0);
+    // Rango HID de modificadores: 224 (0xE0) a 231 (0xE7)
+    return (key_id >= 0xE0 && key_id <= 0xE7);
 }
 
-bool ActionsDriver::check_if_key_is_repeateable(const Key &key)
-{
-    // Check if the key in the key is a modifier key , combination of modifier keys ONLY or a layer shift.
-    // If we found any other key, we return false.
-
-    if (isOnlyModifier(key))
-    {
-        NRF_LOG_DEBUG("Key %i is a modifier key only", key.getRaw());
-        return true;
-    }
-
-    Utils::KeyRanges ranges_t = static_cast<Utils::KeyRanges>(find_key_type(key.getRaw()));
-
-    switch (ranges_t)
-    {
-    case Utils::KeyRanges::LAYER_LOCK:
-    case Utils::KeyRanges::DYNAMIC_MACRO:
-    case Utils::KeyRanges::ALPHA_WITH_MODIFIERS:
-    {
-        NRF_LOG_DEBUG("Key %i is not reapeatable", key.getRaw());
-        return false;
-    }
-    break;
-
-    case Utils::KeyRanges::LAYER_SHIFT:
-    {
-        NRF_LOG_DEBUG("Key %i is reapeatable", key.getRaw());
-        return true;
-    }
-    break;
-
-    default:
-    {
-        NRF_LOG_DEBUG("Key %i is not reapeatable", key.getRaw());
-        return false;
-    }
-    break;
-    }
-}
-
-bool ActionsDriver::check_if_sk_interruptable(const Key &Action)
+bool ActionsDriver::key_can_interrupt(const Key &Action)
 {
     auto ranges_t = static_cast<Utils::KeyRanges>(find_key_type(Action.getRaw()));
     switch (ranges_t)
@@ -119,14 +127,14 @@ bool ActionsDriver::check_if_sk_interruptable(const Key &Action)
     case Utils::KeyRanges::LAYER_SHIFT:
     case Utils::KeyRanges::ALPHA_WITH_MODIFIERS:
     {
-        NRF_LOG_DEBUG("superkey  is NOT interruptable ");
+        // NRF_LOG_DEBUG("Not superkey key  is NOT interruptable ");
         return false;
     }
     break;
 
     default:
     {
-        NRF_LOG_DEBUG("superkey  is interruptable ");
+        // NRF_LOG_DEBUG("Not superkey key  is interruptable ");
         return true;
     }
     break;
@@ -156,4 +164,67 @@ uint16_t ActionsDriver::find_key_type(uint16_t value)
         }
     }
     return -1;
+}
+
+void ActionsDriver::send_regular_key(const Key &key, const KeyAddr &key_addr)
+{
+    uint8_t modif = (key.getRaw() & 0xFF00) >> 8;
+
+    if (key.getRaw() >= 256 && key.getRaw() <= 7935)
+    {
+        if (modif & 0x01)
+        {
+            handleKeyswitchEvent(Key_LeftControl, key_addr, IS_PRESSED | WAS_PRESSED | INJECTED);
+        }
+
+        if (modif & 0x02)
+        {
+            handleKeyswitchEvent(Key_LeftAlt, key_addr, IS_PRESSED | WAS_PRESSED | INJECTED);
+        }
+
+        if (modif & 0x04)
+        {
+            handleKeyswitchEvent(Key_RightAlt, key_addr, IS_PRESSED | WAS_PRESSED | INJECTED);
+        }
+
+        if (modif & 0x08)
+        {
+            handleKeyswitchEvent(Key_LeftShift, key_addr, IS_PRESSED | WAS_PRESSED | INJECTED);
+            NRF_LOG_DEBUG("Left Shift pressed");
+        }
+
+        if (modif & 0x10)
+        {
+            handleKeyswitchEvent(Key_LeftGui, key_addr, IS_PRESSED | WAS_PRESSED | INJECTED);
+        }
+        handleKeyswitchEvent(key, key_addr, IS_PRESSED | WAS_PRESSED | INJECTED);
+    }
+}
+
+void ActionsDriver::send_modifier(const Key &key, const KeyAddr &key_addr)
+{
+    /* Si presionamos un modificador y luego una SK, debemos poder liberar esta SK con el modificador asociado */
+    if (key.getRaw() < Utils::ALPHA_KEYS)
+    {
+        uint8_t modif = (key.getRaw() & 0xFF00) >> 8;
+
+        if (modif & 0x01)
+            handleKeyswitchEvent(Key_LeftControl, key_addr, IS_PRESSED | INJECTED);
+        if (modif & 0x02)
+            handleKeyswitchEvent(Key_LeftAlt, key_addr, IS_PRESSED | INJECTED);
+        if (modif & 0x04)
+            handleKeyswitchEvent(Key_RightAlt, key_addr, IS_PRESSED | INJECTED);
+        if (modif & 0x08)
+            handleKeyswitchEvent(Key_LeftShift, key_addr, IS_PRESSED | INJECTED);
+        if (modif & 0x10)
+            handleKeyswitchEvent(Key_LeftGui, key_addr, IS_PRESSED | INJECTED);
+        if (modif & 0x20)
+            handleKeyswitchEvent(Key_RightControl, key_addr, IS_PRESSED | INJECTED);
+        if (modif & 0x40)
+            handleKeyswitchEvent(Key_RightShift, key_addr, IS_PRESSED | INJECTED);
+        if (modif & 0x80)
+            handleKeyswitchEvent(Key_RightGui, key_addr, IS_PRESSED | INJECTED);
+
+        kaleidoscope::Runtime.hid().keyboard().sendReport();
+    }
 }
--- src/kaleidoscope/plugin/Superkeys/Actions/ActionsDriver.h
+++ src/kaleidoscope/plugin/Superkeys/Actions/ActionsDriver.h
@@ -8,15 +8,29 @@ class Superkey;
 class ActionsDriver
 {
 public:
-    static Key return_type(uint8_t tap_count, const Key *actions, bool repeat_on_hold);
+    static Utils::ExtendedActions return_type(uint8_t tap_count, const Key *actions);
 
-    static bool action_handler(uint8_t tap_count, const Key *actions, const Key &key, const KeyAddr &keyAddr, bool repeat_on_hold);
+    static bool action_handler(uint8_t tap_count, const Key *actions, const Key &key, const KeyAddr &keyAddr);
 
+    /*
+     * 0xE0 (224) → Left Ctrl
+        0xE1 (225) → Left Shift
+        0xE2 (226) → Left Alt
+        0xE3 (227) → Left GUI
+        0xE4 (228) → Right Ctrl
+        0xE5 (229) → Right Shift
+        0xE6 (230) → Right Alt
+        0xE7 (231) → Right GUI
+     * */
     static bool isOnlyModifier(Key key);
 
     static bool check_if_key_is_repeateable(const Key &key);
 
-    static bool check_if_sk_interruptable(const Key &Action);
+    static void send_regular_key(const Key &key, const KeyAddr &keyAddr);
+
+    static bool key_can_interrupt(const Key &Action);
+
+    static void send_modifier(const Key &key, const KeyAddr &key_addr);
 
     static uint16_t find_key_type(uint16_t value);
 };
